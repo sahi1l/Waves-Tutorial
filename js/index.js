21ba7419bfc9886bd603329adc55432dee832fe6
Scott Hill <physhill@gmail.com>
2018-08-05 17:10:48 -0400

    Homepage fixes
    • Fixed header
    • Allows GradeCommand to accept a failure process
    • Add queries to all links

--- js/index.js
+++ js/index.js
@@ -1,29 +1,51 @@
-var getcode=function(){return window.location.search.slice(1);}
+var getcode=function(){return window.location.search.slice(1);};
 var Header = function(data){
     //The process function for the command name
     data=data.trim()
-    if(data=="Unknown"){
-        $("#name").text("I don't recognize your code.");
-    } else {
-        $("#name").text(data);
+    $("#name").text(data);
+};
+var NoHeader=function(){
+    $("#name").text("I don't recognize your code.")
+};
+var GradeCommand = function(input, process, fail) {
+    console.log("GradeCommand: ",input)
+    if(fail==undefined){
+        fail=function(x,txt,b){console.log("Error: ",b);}
     }
-}
-var GradeCommand = function(input, process) {
     $.ajax({
         url:"Grading/grading.cgi",
         dataType:"text",
         method:"POST",
         data:input
-        }).done(process);
+        }).done(process).fail(fail);
 };
 function MarkComplete(ch){
-    return function() {
-        $("li#"+ch.toString()+">.complete").show();
+    return function(data) {
+        if(data.trim()=="True"){
+            $("li#"+ch.toString()+">.complete").show();
+        }
     }
 }
+function addQuery(){
+    console.log($("a"))
+    $("a").each(function(idx,element){
+        console.log(element);
+        var href=element.href;
+        console.log(href);
+        if(href.indexOf("?")<0){
+            element.href=href+"?"+getcode()
+        }
+    });
+}
+
 function init(){
-    GradeCommand({command:"name",code:getcode()},Header);
- 
+    if(getcode()){
+        GradeCommand({command:"name",code:getcode()},
+                     Header,NoHeader);
+        addQuery();
+    } else {
+        $("#name").text("Use the link you were sent via email.");
+    };
     for(i=1;i<=11;i++){
         GradeCommand({command:"complete",code:getcode(),chapter:i},
                  MarkComplete(i));
